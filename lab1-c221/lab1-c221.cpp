bd187a156417a29975174fb38538ff0ed87020bd
ival <ival.spb@gmail.com>
2024-10-01 20:17:38 +0300

    lab1.3.2--

--- lab1-c221/lab1-c221.cpp
+++ lab1-c221/lab1-c221.cpp
@@ -28,19 +28,51 @@ constexpr void InsertUnique(vector<U>& v, T&& current, Types&&... rest)
         InsertUnique(v, forward<Types>(rest)...);
 }
 
+template <typename U, typename T, typename... Types>
+constexpr void functions_with_arg(U& x,  T&& cur_func,  Types&&... rest_functions)
+{
+    forward<T>(cur_func)(x);
+    if constexpr (sizeof...(rest_functions) > 0)
+        functions_with_arg(x, forward<Types>(rest_functions)...);
+}
+
+template <typename U, typename T, typename... Types>
+constexpr void functions_with_arg_folded(U& x, Types&&... functions)
+{
+    (forward<Types>(functions)(x), ...);
+}
+
+
+//template <typename C::value_type=T, typename T >
+
+template <typename T> 
+void decr_any(T& t)
+{
+    t--;
+}
+
 int main()
 {
     int a = 1, b = 2, c = 3;
-    vector v = { ref(a),ref(b),ref(c) };
+    vector<std::reference_wrapper<int>> v = { ref(a),ref(b),ref(c) };
     fV(v); //в результате вызова fV a,b,c должны измениться!!!
-
+    
     vector v1{ 4,7, 1 };
     InsertUnique(v1, 1.1f, string("qwerty"), 5.2, "qwerty", 'c');
 
+    double d1 = 1.1, d2 = 2.2, d3 = 3.3;
+    double delta = 3;
+    
+
+    functions_with_arg(d1, [delta](auto& d) {d += delta; },decr_any<double>);
+
+    functions_with_arg_folded(d3, [delta](auto& d) {d -= delta; }, decr_any<double>);
+
+
     //требуется определить сколько элементов совпадающих с перечисленными в паке значениями содержит последовательность (обобщенный контейнер).
-    vector v{ 1,2,3,4,5 };
+    /*vector v{ 1,2,3,4,5 };
     size_t n1 = mathches(v, 2, 5);
     size_t n2 = mathches(v, 100, 200);
     size_t n3 = mathches("abcdefg", 'x', 'y', 'z');
-    size_t n4 = mathches("abcdefg", 'a', 'd', 'f');
+    size_t n4 = mathches("abcdefg", 'a', 'd', 'f');*/
 }
