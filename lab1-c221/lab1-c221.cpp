836fbb435485f20ef9eafbe9ca48edcce15a2b4d
ivalspb <133792774+ivalspb@users.noreply.github.com>
2024-10-03 17:13:27 +0300

    Update lab1-c221.cpp

--- lab1-c221/lab1-c221.cpp
+++ lab1-c221/lab1-c221.cpp
@@ -31,14 +31,14 @@ constexpr void InsertUnique(vector<U>& v, T&& current, Types&&... rest)
 }
 
 template <typename U, typename T, typename... Types>
-constexpr void functions_with_arg(U& x,  T&& cur_func,  Types&&... rest_functions)
+constexpr void functions_with_arg(U& x, T&& cur_func, Types&&... rest_functions)
 {
     forward<T>(cur_func)(x);
     if constexpr (sizeof...(rest_functions) > 0)
         functions_with_arg(x, forward<Types>(rest_functions)...);
 }
 
-template <typename U,  typename... Types>
+template <typename U, typename... Types>
 constexpr void functions_with_arg_folded(U& x, Types&&... functions)
 {
     (forward<Types>(functions)(x), ...);
@@ -49,10 +49,10 @@ constexpr void func_any_param(U&& func, T&& cur, Types&&... rest)
 {
     forward<U>(func)(cur);
     if constexpr (sizeof...(rest) > 0)
-        func_any_param(forward<U>(func),forward<Types>(rest)...);
+        func_any_param(forward<U>(func), forward<Types>(rest)...);
 }
 
-template <typename U,  typename... Types>
+template <typename U, typename... Types>
 constexpr void func_any_param_folded(U&& func, Types&&... rest)
 {
     (forward<U>(func)(forward<Types>(rest)), ...);
@@ -61,7 +61,7 @@ constexpr void func_any_param_folded(U&& func, Types&&... rest)
 
 //template <typename C::value_type=T, typename T >
 
-template <typename T> 
+template <typename T>
 void decr_any(T& t)
 {
     t--;
@@ -72,15 +72,15 @@ int main()
     int a = 1, b = 2, c = 3;
     vector<std::reference_wrapper<int>> v = { ref(a),ref(b),ref(c) };
     fV(v); //в результате вызова fV a,b,c должны измениться!!!
-    
+
     vector v1{ 4,7, 1 };
     InsertUnique(v1, 1.1f, string("qwerty"), 5.2, "qwerty", 'c');
 
     double d1 = 1.1, d2 = 2.2, d3 = 3.3;
     double delta = 3;
-    
 
-    functions_with_arg(d1, [delta](auto& d) {d += delta; },decr_any<double>);
+
+    functions_with_arg(d1, [delta](auto& d) {d += delta; }, decr_any<double>);
     functions_with_arg_folded(d3, [delta](auto& d) {d -= delta; }, decr_any<double>);
 
     func_any_param(decr_any<double>, d1, d2, d3);
@@ -92,4 +92,11 @@ int main()
     size_t n2 = mathches(v, 100, 200);
     size_t n3 = mathches("abcdefg", 'x', 'y', 'z');
     size_t n4 = mathches("abcdefg", 'a', 'd', 'f');*/
+
+    MyStack<Point<int>> s;
+    Point<int> pt(1, 1);
+    s.push(pt); //добавить в стек копию lvalue
+    s.push(Point(2, 2)); //переместить в стек из rvalue
+
+
 }
