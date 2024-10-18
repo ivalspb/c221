0fbd119b5160f929e4ec0d87df0331393d47c5c7
ival <ival.spb@gmail.com>
2024-10-18 08:06:24 +0300

    str derived vector function begin

--- lab3-c221/lab3-c221.cpp
+++ lab3-c221/lab3-c221.cpp
@@ -3,6 +3,9 @@
 #include <array>
 #include <algorithm>
 #include <cstdlib>
+#include <string_view>
+#include <string>
+#include <set>
 
 int main()
 {
@@ -25,5 +28,24 @@ int main()
 	{
 		std::cout << x<<"\tSquare = " << x.getSq() << std::endl;
 	}
+
+	const char* str[]{ "123", "abc", "qwerty" };
+	char ar1[] = "dog";
+	char* p = new char[] {"cat"};
+	std::string s("great");
+	//Требуется отсортировать все строки, не дублируя (не копируя) содержимое
+	std::set<std::string_view>sorted_strings;
+	sorted_strings.insert(std::begin(str), std::end(str));
+	sorted_strings.insert(ar1);
+	sorted_strings.insert(p);
+	sorted_strings.insert(s);
+	for (const auto& i : sorted_strings)
+		std::cout << std::endl << i;
+
+	//Реализуйте функцию, которая «разбивает» исходную строку на отдельные слова и 
+	// создает из этих строк std::vector  - "abc”,  “12345”, “cat”, “dog”, “bird". 
+	// Строки копировать нельзя!
+	const char* s = "abc 12345 cat dog bird";
+
 }
 
---
+++ lab3-c221/str_derived_vector.h
@@ -0,0 +1,9 @@
+#pragma once
+#include <vector>
+#include <string_view>
+
+template <typename... Types>
+std::vector<std::string_view> make_str_vec(Types&&... args)
+{
+
+}
